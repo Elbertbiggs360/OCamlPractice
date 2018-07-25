13354a6dcd3963513b5471c30861e77a1fc491bd
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-07-25 10:30:53 +0300

    Lesson 2

---
+++ lesson2.ml
@@ -0,0 +1,16 @@
+let x=6*7;;
+x;;
+let y=x+1;;
+x &lt;- x+1;;
+let x = ref 42;;
+x := 100/4;;
+x;;
+let y = !x + 1;;
+print_int 3;;
+print_string "Hello";;
+print_string "x = ";;
+;
+print_string "x = ";;
+print_int !x;;
+print_newline();;
+Printf.printf "x = %d. Bye %s\n" !x "John";;
